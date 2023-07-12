c11c27f1f082545dc62f800d2ed8d6a0281927fa
straussdave <101413861+straussdave@users.noreply.github.com>
2023-07-12 10:20:36 +0200

    added images and report generation

--- TourPlanner1/ViewModel/MainViewModel.cs
+++ TourPlanner1/ViewModel/MainViewModel.cs
@@ -9,20 +9,60 @@ using System.Diagnostics;
 using System.Windows.Input;
 using TourPlanner1.View;
 using System.Windows;
+using System.Collections.Generic;
+using TourPlanner1.Model.Messages;
+using System.Linq;
+using CommunityToolkit.Mvvm.Messaging;
 
 namespace TourPlanner1.ViewModel
 {
-    public partial class MainViewModel : ObservableObject
+    public partial class MainViewModel : ObservableRecipient
     {
+        IConfig config = new Model.Config();
+        ReportGenerator reportGenerator = new();
+
+        [ObservableProperty]
+        string routeImage;
+
+        [ObservableProperty]
+        private Tour selectedTour;
+
+        [ObservableProperty]
+        private List<Tour> tourList;
+
         public MainViewModel()
         {
-
+            DatabaseHandler databaseHandler = new DatabaseHandler(new TourPlannerDbContext(), config);
+            TourList = databaseHandler.ReadTours();
+            SelectedTour = TourList.FirstOrDefault(defaultValue: null);
+            Messenger.Register<MainViewModel, TourSelectedMessage>(this, (r, m) => {
+                SelectedTour = m.Selected;
+                RouteImage = PathHelper.GetBasePath() + "\\Images\\" + SelectedTour.RouteImage;
+            });
         }
 
         [RelayCommand]
         void CloseApplication()
         {
             Application.Current.MainWindow.Close();
+        }
+
+        void OpenErrorWindow(string message)
+        {
+            ErrorWindow errorWindow = new(message);
+            errorWindow.Show();
+        }
+
+        [RelayCommand]
+        void GenerateReport()
+        {
+            reportGenerator.GenerateReport(SelectedTour);
+        }
+
+        [RelayCommand]
+        void GenerateSummaryReport()
+        {
+            reportGenerator.GenerateSummaryReport();
         }
     }
 }
